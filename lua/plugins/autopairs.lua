7af1a85f06593a05886a570d5c77864291a3cfb1
Cangsong W <cwang40@slb.com>
2024-04-11 12:54:56 +0800

    fix auto pairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,50 +1,45 @@
+if true then return {} end
+
 return {
   "windwp/nvim-autopairs",
+  event = "InsertEnter",
   opts = {
     ignored_next_char = "[%w%.%(%{%[]",
     fast_wrap = {
       chars = { "{", "[", "(", '"', "'", "`" },
-    }
+    },
   },
   config = function(plugin, opts)
-    require "plugins.configs.nvim-autopairs" (plugin, opts)
+    require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts)
 
     local npairs = require "nvim-autopairs"
     local Rule = require "nvim-autopairs.rule"
     local cond = require "nvim-autopairs.conds"
 
-    local function move_with_end_pair(opts)
-      return opts.char == opts.rule.end_pair
-    end
+    local function move_with_end_pair(opts) return opts.char == opts.rule.end_pair end
 
-    npairs.add_rules(
-      {
-        Rule("<", ">", "rust")
-            :with_pair(cond.not_after_regex("%w"))
-            :with_pair(cond.before_text("::"))
-            :with_cr(cond.none())
-            :with_move(move_with_end_pair)
-      }
-    )
-    npairs.add_rules(
-      {
-        Rule("<", ">", "rust")
-            :with_pair(cond.not_after_regex("%w"))
-            :with_pair(cond.before_regex("%w"))
-            :with_cr(cond.none())
-            :with_move(move_with_end_pair)
-      }
-    )
+    npairs.add_rules {
+      Rule("<", ">", "rust")
+        :with_pair(cond.not_after_regex "%w")
+        :with_pair(cond.before_text "::")
+        :with_cr(cond.none())
+        :with_move(move_with_end_pair),
+    }
+    npairs.add_rules {
+      Rule("<", ">", "rust")
+        :with_pair(cond.not_after_regex "%w")
+        :with_pair(cond.before_regex "%w")
+        :with_cr(cond.none())
+        :with_move(move_with_end_pair),
+    }
 
-    npairs.add_rules(
-      {
-        Rule("<", ">", "rust")
-            :with_pair(cond.before_text("<"))
-            :with_pair(cond.after_text(">"))
-            :with_pair(cond.not_after_regex("%w"))
-            :with_cr(cond.none())
-            :with_move(move_with_end_pair)
-      }
-    )
+    npairs.add_rules {
+      Rule("<", ">", "rust")
+        :with_pair(cond.before_text "<")
+        :with_pair(cond.after_text ">")
+        :with_pair(cond.not_after_regex "%w")
+        :with_cr(cond.none())
+        :with_move(move_with_end_pair),
+    }
   end,
 }
