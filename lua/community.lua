e97a848f4d6b9aa8ace5b392a47489d21220085e
Cangsong Wang <knairwang@outlook.com>
2024-07-23 15:14:46 +0800

    update

--- lua/community.lua
+++ lua/community.lua
@@ -9,7 +9,7 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.cs" },
-  { import = "astrocommunity.pack.elixir-phoenix" },
+  -- { import = "astrocommunity.pack.elixir-phoenix" },
   { import = "astrocommunity.pack.gleam" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.json" },
