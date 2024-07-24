1f181db15bd9ab1920bd325b394e77324a4c0b06
Cangsong Wang <knairwang@outlook.com>
2024-07-24 21:51:47 +0800

    disable autopairs customization

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,31 +1,48 @@
--- if true then return {} end
+if true then return {} end
 
-return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  opts = {
-    ignored_next_char = "[%w%.%(%{%[<]",
-    fast_wrap = {
-      chars = { "{", "[", "(", '"', "'", "`" },
-    },
-  },
-  config = function(plugin, opts)
-    require "astronvim.plugins.configs.nvim-autopairs" (plugin, opts)
+--
+-- The > key mapping is conflict with ts-autotag
+-- thus disable this customization
 
-    -- local npairs = require "nvim-autopairs"
-    -- local Rule = require "nvim-autopairs.rule"
-    -- local cond = require "nvim-autopairs.conds"
+-- return {
+--   "windwp/nvim-autopairs",
+--   -- event = "InsertEnter",
+--   -- enabled = true,
+--   -- opts = {
+--   --   enabled = true,
+--   --   -- ignored_next_char = "[%w%.%(%{%[<]",
+--   --   fast_wrap = {
+--   --     chars = { "{", "[", "(", '"', "'", "`" },
+--   --   },
+--   -- },
+--   config = function(plugin, opts)
+--     require "astronvim.plugins.configs.nvim-autopairs" (plugin, opts)
 
-    -- npairs.add_rules {
-    --   Rule("<", ">", "rust")
-    --       :with_pair(cond.before_text("::"), 2)
-    --       :with_move(function(op) return op.char ~= "<" end)
-    -- }
+--     local npairs = require 'nvim-autopairs'
+--     local Rule = require 'nvim-autopairs.rule'
+--     local cond = require 'nvim-autopairs.conds'
 
-    -- npairs.add_rules {
-    --   Rule("<", ">", "rust")
-    --       :with_pair(cond.before_regex("%w"), 1)
-    --       :with_move(function(op) return op.char ~= "<" end)
-    -- }
-  end,
-}
+--     local start_char = '<'
+--     local end_char = '>'
+--     npairs.add_rules({
+--       -- Rule(start_char, end_char, "rust")
+--       --     :with_pair(cond.none())
+--       --     :with_move(function(op) return op.char == end_char end)
+--       --     :use_key(end_char)
+--       --     :with_del(cond.none()),
+
+--       -- Rule(start_char, end_char, "rust")
+--       --     :with_pair(cond.before_text("::"), 2)
+--       --     :with_move(function(op) return op.char == end_char end)
+--       --     :with_del(cond.done()),
+
+--       Rule(start_char, end_char, "rust")
+--           :with_pair(cond.before_regex("%w"), 1)
+--           :with_move(function(op)
+--             return op.char == end_char
+--           end)
+--           :with_del(cond.done()),
+--           -- .use_key(end_char)
+--     })
+--   end,
+-- }
