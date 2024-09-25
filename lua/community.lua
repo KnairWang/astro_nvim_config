75c389630501d8495078b116f3004cdaab062990
Cangsong Wang <cwang40@slb.com>
2024-09-25 11:19:12 +0800

    dynamically enable packs

--- lua/community.lua
+++ lua/community.lua
@@ -12,14 +12,9 @@ local community_imports = {
   -- { import = "astrocommunity.recipes.vscode" },
   { import = "astrocommunity.motion.nvim-surround" },
   -- { import = "astrocommunity.pack.cs" },
-  { import = "astrocommunity.pack.gleam" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.toml" },
-  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.xml" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.recipes.vscode-icons" },
@@ -30,6 +25,25 @@ if vim.fn.executable("go") == 1 then
   table.insert(community_imports, { import = "astrocommunity.pack.go" })
 end
 
+if vim.fn.executable("gleam") == 1 then
+  table.insert(community_imports, { import = "astrocommunity.pack.gleam" })
+end
+
+if vim.fn.executable("python") == 1 or vim.fn.executable("python3") == 1 then
+  table.insert(community_imports, { import = "astrocommunity.pack.python" })
+end
+
+if vim.fn.executable("rustup") == 1 and vim.fn.executable("cargo") then
+  table.insert(community_imports, { import = "astrocommunity.pack.rust" })
+end
+
+if vim.fn.executable("node") == 1 then
+  table.insert(community_imports, { import = "astrocommunity.pack.typescript" })
+end
+
+if vim.fn.executable("lua") == 1 then
+  table.insert(community_imports, { import = "astrocommunity.pack.lua" })
+end
 -- if vim.fn.executable("elixir") == 1 and vim.fn.executable("iex") == 1 then
 --   table.insert(community_imports, { import = "astrocommunity.pack.elixir-phoenix" })
 -- end
