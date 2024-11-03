68058d09c0606c8b6719e6567cf54b02fbc754d9
Cangsong Wang <knairwang@outlook.com>
2024-11-03 21:02:13 +0800

    enable telescop lsp mappings

--- lua/community.lua
+++ lua/community.lua
@@ -9,6 +9,7 @@
 ---@type LazySpec
 local community_imports = {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
   -- { import = "astrocommunity.recipes.vscode" },
   { import = "astrocommunity.motion.nvim-surround" },
   -- { import = "astrocommunity.pack.cs" },
